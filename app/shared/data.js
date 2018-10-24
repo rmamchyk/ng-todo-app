e94be3e4c6d1e97d658e3e187376d1225f3adb14
rmamchyk <roman.mamchyk@gmail.com>
2018-10-25 00:24:47 +0300

    Removed *.js and *.js.map files

--- app/shared/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var todo_1 = require("./todo");
-exports.todos = [
-    new todo_1.Todo('Learn JavaScript', true),
-    new todo_1.Todo('Learn Angular'),
-    new todo_1.Todo('Create an app')
-];
-//# sourceMappingURL=data.js.map
